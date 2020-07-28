e765f4abff72ad3d8082f2224afee9dae13480b2
Carolyn Ryan <ryanc2@carleton.edu>
2020-07-27 18:27:06 -0600

    Formatting to make things more consistent

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Player Characters</h1>
     <div v-for="character in characters" :key="character.name" class="character-data">
-      <div class="character-stats">
+    <div class="character-stats">
         <div>
           <span>{{character.race}}, {{character.classes[0].name}}</span>
         </div>
@@ -18,12 +18,44 @@
           <span class="money">  Gold: {{character.currencies["gp"]}}</span>
         </div>
         <div class="money">
-            <span>Languages: {{character.languages}}</span>
+            <span>Language: </span>
+            <span v-for="language in character.languages">{{language}}, </span>
         </div>
         <div>
-            <span> Spell Saves: {{character.spellcastingInfo.spellSaveDifficulties[0]}}, </span>
-            <span> Spell Attack Bonus: {{character.spellcastingInfo.spellAttackBonuses[0]}}, </span>
-            <span> Spellcasting Abilities: {{character.spellcastingInfo.spellcastingAbilities[0]}}</span>
+            <span v-if="character.spellcastingInfo.spellSaveDifficulties[0]"> Spell Saves: {{character.spellcastingInfo.spellSaveDifficulties[0]}}</span>
+        </div>
+        <div>
+            <span v-if="character.spellcastingInfo.spellAttackBonuses[0]"> Spell Attack Bonus: {{character.spellcastingInfo.spellAttackBonuses[0]}}</span>
+        </div>
+        <div>
+            <span v-if="character.spellcastingInfo.spellcastingAbilities[0]"> Spellcasting Abilities: {{character.spellcastingInfo.spellcastingAbilities[0]}}</span>
+        </div>
+        <div>
+            <span>Notes</span>
+            <ul>
+                <li v-if="character.notes.allies">Allies: {{character.notes.allies}}</li>
+                <li v-if="character.notes.backstory">Backstory: {{character.notes.backstory}}</li>
+                <li v-if="character.notes.enemies">Enemies: {{character.notes.enemies}}</li>
+                <li v-if="character.notes.organizations">Organizations: {{character.notes.organizations}}</li>
+                <li v-if="character.notes.otherNotes">Other notes: {{character.notes.otherNotes}}</li>
+                <li v-if="character.notes.otherHoldings">Other holdings: {{character.notes.otherHoldings}}</li>
+                <li v-if="character.notes.personalPossessions">Personal possessions: {{character.notes.personalPossessions}}</li>
+            </ul>
+        </div>
+        <div>
+            <span>Characteristics</span>
+            <ul>
+                <li v-if="character.characteristics.age">Age: {{character.characteristics.age}}</li>
+                <li v-if="character.characteristics.height">Height: {{character.characteristics.height}}</li>
+                <li v-if="character.characteristics.weight">Weight: {{character.characteristics.weight}}</li>
+                <li v-if="character.characteristics.eyes">Eyes: {{character.characteristics.eyes}}</li>
+                <li v-if="character.characteristics.skin">Skin: {{character.characteristics.skin}}</li>
+                <li v-if="character.characteristics.hair">Hair: {{character.characteristics.hair}}</li>
+                <li v-if="character.characteristics.size">Size: {{character.characteristics.size}}</li>
+                <li v-if="character.characteristics.faith">Faith: {{character.characteristics.faith}}</li>
+                <li v-if="character.characteristics.gender">Gender: {{character.characteristics.gender}}</li>
+                <li v-if="character.characteristics.alignment">Alignment: {{character.characteristics.alignment}}</li>
+            </ul>
         </div>
       </div>
       <div class="character-stats">
@@ -81,11 +113,11 @@ export default {
 </script>
 
 
+
 <style scoped>
 
 .character-data {
   display: flex;
-  align-items: center;
   margin-top: 20px;
   margin-left: 20px;
   border-bottom: 2px solid #ccc;
@@ -96,6 +128,7 @@ export default {
   flex-grow: 8;
   text-align: left;
   padding-left: 20px;
+  max-width: 30%;
 }
 
 .character-stats .name {
@@ -143,5 +176,35 @@ th {
 
 tr:nth-child(even) {
   background-color: #dddddd;
+}
+
+ul {
+  margin-left: 20px;
+}
+
+/* Style the button that is used to open and close the collapsible content */
+.collapsible {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active, .collapsible:hover {
+  background-color: #ccc;
+}
+
+/* Style the collapsible content. Note: hidden by default */
+.content {
+  padding: 0 18px;
+  display: none;
+  overflow: hidden;
+  background-color: #f1f1f1;
 }
 </style>
