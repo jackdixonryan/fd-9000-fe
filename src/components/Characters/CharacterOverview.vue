0c6788af34c3af78ea17753b2678fa4693e094bc
Jack Ryan <jack.dixon.ryan@gmail.com>
2020-07-29 10:09:54 -0600

    Overview on character page is revised to an expandable nav style. Only top-level styling has been completed.

---
+++ src/components/Characters/CharacterOverview.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="character-overview">
+    <div class="header-panel">
+      <div class="character-name-div">
+        <h1 class="character-name">
+          <span>
+            <button class="expand-button">+</button>  
+          </span>
+          {{ character.name }}
+        </h1>
+      </div>
+      <div class="character-class">
+        {{ character.race }} | Level {{ character.classes[0].level }} {{ character.classes[0].name }}
+      </div>
+      <div class="armor-class">
+        {{ character.armorClass }}
+      </div>
+      <div class="passive-perception">
+        {{ character.passivePerception }}
+      </div>
+      <div class="hit-points">
+        {{ character.hitPointMax }}
+      </div>
+      <div class="character-speed">
+        <span v-for="speed in character.speeds" :key="speed.name + character.name">
+          <!-- only render speeds the character has -->
+          <span v-if="speed.distance > 0">
+            {{ speed.name }} : {{ speed.distance }}ft
+          </span>
+        </span>
+      </div>
+      <button class="action-button">
+        <i class="fas fa-magic"></i>
+      </button>
+      <button class="action-button">
+        <i class="fas fa-fist-raised"></i>
+      </button>
+      <button class="action-button">
+        <i class="fas fa-dice-d20"></i>
+      </button>
+      <button class="action-button">
+        <i class="fas fa-search"></i>
+      </button>
+      <input type="text" :name="character.name + '-search'" :id="character.name + '-search'" placeholder="search" class="text-input" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["character"]
+  }
+</script>
+
+<style lang="scss" scoped>
+  .character-overview {
+    width: 100%;
+    font-family: "Fira Sans", sans-serif;
+  }
+
+  .character-name {
+    font-size: 1.5rem;
+  }
+
+  .coin {
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    display: inline-block;
+  }
+
+  .gold {
+    background-color: gold;
+  }
+
+  .silver {
+    background-color: silver;
+  }
+
+  .copper {
+    background-color: burlywood;
+  }
+
+  .expand-button {
+    font-family: "Fira Sans", sans-serif;
+    font-size: 1.5rem;
+    background: none;
+    border: none;
+    outline: none;
+    color: #E85A4F;
+    cursor: pointer;
+  }
+
+  .character-name-div {
+    display: inline-block;
+  }
+
+  .character-class {
+    display: inline-block;
+    margin-left: 2rem;
+  }
+
+  .armor-class {
+    display: inline-block;
+    margin-left: 2rem;
+    background-color: #D8C3A5; 
+    padding: 1rem;
+    border-radius: 50%;
+    font-size: 1rem;
+  }
+
+  .passive-perception {
+    display: inline-block;
+    margin-left: 2rem;
+    background-color: #D8C3A5; 
+    padding: 1rem;
+    border-radius: 50%;
+    font-size: 1rem;
+  }
+
+  .hit-points {
+    display: inline-block;
+    margin-left: 2rem;
+    background-color: #E85A4F; 
+    padding: 1rem;
+    border-radius: 50%;
+    font-size: 1rem;
+    color: #EAE7DC;
+  }
+
+  .character-speed {
+    display: inline-block;
+    margin-left: 2rem;
+  }
+
+  .action-button {
+    color: #E85A4F;
+    background-color: #EAE7DC;
+    padding: 1rem;
+    display: inline-block;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin-left: 2rem;
+  }
+
+  .text-input {
+    background: none;
+    margin: none;
+    border: none;
+    font-size: 1rem;
+    color: #E85A4F;
+    border-bottom: 1px #E85A4F solid;
+    outline: none;
+    padding: 0.25rem;
+    &:focus {
+      padding: 0.5rem;
+    }
+  }
+
+</style>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,65 +1,10 @@
 <template>
   <div>
-    <h1>Player Characters</h1>
+    <h1 class="title">Your Characters</h1>
     <div v-for="character in characters" :key="character.name" class="character-data">
-    <div class="character-stats">
-        <div>
-          <span>{{character.race}}, {{character.classes[0].name}}</span>
-        </div>
-        <div>
-          <span class="name">{{character.name}}</span>
-        </div>
-        <div class="character-armor-class">
-            <span>AC: {{character.armorClass}} PP: {{character.passivePerception}} HP: {{character.hitPointMax}}</span>
-        </div>
-        <div>
-          <span class="money"> Copper: {{character.currencies["cp"]}}</span>
-          <span class="money">  Silver: {{character.currencies["sp"]}}</span>
-          <span class="money">  Gold: {{character.currencies["gp"]}}</span>
-        </div>
-        <div class="money">
-            <span>Language: </span>
-            <span v-for="language in character.languages">{{language}}, </span>
-        </div>
-        <div>
-            <span v-if="character.spellcastingInfo.spellSaveDifficulties[0]"> Spell Saves: {{character.spellcastingInfo.spellSaveDifficulties[0]}}</span>
-        </div>
-        <div>
-            <span v-if="character.spellcastingInfo.spellAttackBonuses[0]"> Spell Attack Bonus: {{character.spellcastingInfo.spellAttackBonuses[0]}}</span>
-        </div>
-        <div>
-            <span v-if="character.spellcastingInfo.spellcastingAbilities[0]"> Spellcasting Abilities: {{character.spellcastingInfo.spellcastingAbilities[0]}}</span>
-        </div>
-        <div>
-            <span>Notes</span>
-            <ul>
-                <li v-if="character.notes.allies">Allies: {{character.notes.allies}}</li>
-                <li v-if="character.notes.backstory">Backstory: {{character.notes.backstory}}</li>
-                <li v-if="character.notes.enemies">Enemies: {{character.notes.enemies}}</li>
-                <li v-if="character.notes.organizations">Organizations: {{character.notes.organizations}}</li>
-                <li v-if="character.notes.otherNotes">Other notes: {{character.notes.otherNotes}}</li>
-                <li v-if="character.notes.otherHoldings">Other holdings: {{character.notes.otherHoldings}}</li>
-                <li v-if="character.notes.personalPossessions">Personal possessions: {{character.notes.personalPossessions}}</li>
-            </ul>
-        </div>
-        <div>
-            <span>Characteristics</span>
-            <ul>
-                <li v-if="character.characteristics.age">Age: {{character.characteristics.age}}</li>
-                <li v-if="character.characteristics.height">Height: {{character.characteristics.height}}</li>
-                <li v-if="character.characteristics.weight">Weight: {{character.characteristics.weight}}</li>
-                <li v-if="character.characteristics.eyes">Eyes: {{character.characteristics.eyes}}</li>
-                <li v-if="character.characteristics.skin">Skin: {{character.characteristics.skin}}</li>
-                <li v-if="character.characteristics.hair">Hair: {{character.characteristics.hair}}</li>
-                <li v-if="character.characteristics.size">Size: {{character.characteristics.size}}</li>
-                <li v-if="character.characteristics.faith">Faith: {{character.characteristics.faith}}</li>
-                <li v-if="character.characteristics.gender">Gender: {{character.characteristics.gender}}</li>
-                <li v-if="character.characteristics.alignment">Alignment: {{character.characteristics.alignment}}</li>
-            </ul>
-        </div>
+      <character-overview :character="character"></character-overview>
         
-      </div>
-      <div class="character-stats">
+      <!-- <div class="character-stats">
         <template>
             <div>
                 <vue-good-table
@@ -84,15 +29,19 @@
 
             </div>
         </template>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 
 <script>
+import CharacterOverview from "../components/Characters/CharacterOverview"
 export default {
-  name: 'my-component',
+  name: 'characters',
+  components: {
+    CharacterOverview
+  },
   data(){
     return {
       spellColumns: [
@@ -181,10 +130,8 @@ export default {
 
 .character-data {
   display: flex;
-  margin-top: 20px;
-  margin-left: 20px;
-  border-bottom: 2px solid #ccc;
-  padding: 20px;
+  margin-left: 1.5rem;
+  padding: 1.3rem;
 }
 
 .character-stats {
@@ -212,13 +159,6 @@ img {
   width: 70px;
 }
 
-h1 {
-    padding:10px;
-    background-color: #1aa832;
-    color: white;
-    border: 1px solid #ccc;
-    font-size: 40px;
-}
 
 table {
   font-family: arial, sans-serif;
@@ -269,5 +209,11 @@ ul {
   display: none;
   overflow: hidden;
   background-color: #f1f1f1;
+}
+
+.title {
+  padding: 1.5rem;
+  color: #E85A4F;
+  font-size: 2rem;
 }
 </style>
